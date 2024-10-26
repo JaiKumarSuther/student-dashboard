05bdd57c6949c5820b063f25d90a06786643a851
Jai Kumar <116871953+JaiKumarSuther@users.noreply.github.com>
2024-10-26 15:24:15 +0500

    adjusted the profile-section

--- src/app/components/leaderboard/leaderboard.component.ts
+++ src/app/components/leaderboard/leaderboard.component.ts
@@ -1,12 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-leaderboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './leaderboard.component.html',
   styleUrl: './leaderboard.component.css'
 })
 export class LeaderboardComponent {
+  students = [
+    {
+      name: 'Student 1',
+      grade: 'grade',
+      score: '12345',
+      background: '#FFF0E1',
+    },
+    {
+      name: 'Student 2',
+      grade: 'grade',
+      score: '12345',
+      background: '#E9E3FF'
 
+    },
+    {
+      name: 'Student 3',
+      grade: 'grade',
+      score: '12345',
+      background: '#E5F3FF'
+    },
+    {
+      name: 'Student 4',
+      grade: 'grade',
+      score: '12345',
+      background: '#FFF0E1',
+    }
+  ]
 }
