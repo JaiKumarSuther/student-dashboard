dbf1614950587c51b58bbd984c2b19cbb0db5a16
Jai Kumar <116871953+JaiKumarSuther@users.noreply.github.com>
2024-10-28 00:33:59 +0500

    added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DashboardOverviewComponent } from './components/dashboard-overview/dashboard-overview.component';
+import { CoursesOverviewComponent } from './components/courses-overview/courses-overview.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'dashboard', component: DashboardOverviewComponent },
+  { path: 'courses', component: CoursesOverviewComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // Default route
+];
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { SubscriptionBannerComponent } from './subscription-banner/subscription-banner.component';
+import { Router, RouterModule } from '@angular/router';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [SubscriptionBannerComponent],
+  imports: [SubscriptionBannerComponent, RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+  constructor(private router: Router) {}
 
+  navigateTo(route: string) {
+    this.router.navigate([route]);
+  }
 }
